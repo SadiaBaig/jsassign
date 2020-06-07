b5ae19334ba8770dfb0b19bac4568c56e3278a13
SadiaBaig <57152119+SadiaBaig@users.noreply.github.com>
2020-06-07 14:04:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+// alert("welcome here"); //Question1 chap1
+// alert("enter valid password 1"); //Question2 chap1
+// alert("welcome to js land\n happy coding"); //Question3 chap1
+// alert("welcome to js land"); //Question4 chap1
+// alert("hello...I can run through my web browser console"); //question5 chap 1
+// var username;//Question1 chap2
+// username = "sadia";//Question2 chap2
+// alert("john doe"); //Question4 chap2
+// alert("15 year old"); //Question4 chap2
+// alert("Certified mobile app development"); //Question4 chap2
+// for (var i=0; i<10; i++){
+//     for (var j=0; j<i; j++){
+// document.write("pizza")
+//     }
+// document.write("<br>")
+// }//question5 chap2
+// var email; //Question6 chap2
+// email = alert("my email add is e.g@e.g.com"); //Question6 chap2
+// document.write("yah ! I can write Html content through js");//Question8 chap2
+// var book; //Question7 chap2
+// book = alert("I am trying to learn js in smarter way");//Question7 chap2
+// var num = 10;Question1 chap5
+// var anotherNum = 1;Question1 chap5
+// var popularNumber = num + anotherNum;Question1 chap5
+// document.write(popularNumber);Question1 chap5
+// for (var i = 1; i <= 10; i++) {Question5 chap5
+//     document.write("4" + "x" + i + "=" + 2 * i + "<br>")Question5 chap5
+// }Question5 chap5
+// for (var i = 1; i <= 10; i++) {----Question3  chap17
+//     document.write(i + "<br>")-----Question3  chap17
+// }-----Question3  chap17
+// var arry = [];//chap17 question1
+// var a = 2,----chap 6-9 question 2
+//     b = 1; ----chap 6-9 question 2
+// var result = --a - --b + ++b + b--; ----chap 6-9 question 2
+// --a; --a - --b; --a - --b + ++b; --a - --b + ++b + b--; ----chap 6-9 question 2 
+// ans = -3 ----chap 6-9 question 2ss
